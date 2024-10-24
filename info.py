603be4d96d73dba444819d284f353c449f3e5ddb
Ganesh <100681292+Ganeshmrxx@users.noreply.github.com>
2024-10-24 15:44:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,6 +32,6 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://test:test05@cluster0.g
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
